9d93eef1e201ae3dc3002cef142b215b127eb155
prim1112 <65011212063@msu.ac.th>
2025-09-17 16:26:17 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,10 +24,6 @@ class MyApp extends StatelessWidget {
       ),
 
       home: OdrawPage(),
-
-      home: ShoppingPage(), //เปิดหน้า Login
-
-      home: ShoppingPage(), //เปิดหน้า Login
     );
   }
 }
