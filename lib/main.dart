03e6183778c1a15428560aa2eccfa5d8e9c0dd89
prim1112 <65011212063@msu.ac.th>
2025-09-20 10:24:54 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
-=======
 import 'package:lotto_application/pages/customer/homepage.dart';
->>>>>>> Stashed changes
 import 'package:lotto_application/pages/customer/shopping_bag.dart';
 import 'package:lotto_application/pages/login.dart';
 import 'package:lotto_application/pages/owner/Owner_draw.dart';
@@ -22,12 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-<<<<<<< Updated upstream
-
-      home: ShoppingPage(),
-=======
-      home: ShoppingPage(), //เปิดหน้า Login
->>>>>>> Stashed changes
+      home: LoginPage(), //เปิดหน้า Login
     );
   }
 }
